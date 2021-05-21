794b5f9ca80f74a2260ed17c88b7a00108da5cc6
M Usama Alvi <m.usamaalvi@gmail.com>
2021-05-22 04:21:51 +0500

    Section Conditions

--- StatementsSequences/StatementsSequences/Program.cs
+++ StatementsSequences/StatementsSequences/Program.cs
@@ -8,7 +8,88 @@ namespace StatementsSequences
         {
 
 
-            NDiscount();
+            ConvertToMonth();
+        }
+        static void ConvertToMonth()
+        {
+            Console.Write("Enter Month number");
+            int month = Convert.ToInt32(Console.ReadLine());
+            DateTime dt = new DateTime(2021, month, 1);
+            Console.WriteLine(dt.ToString("MMM"));
+        }
+        static void LargestThreeNumbers()
+        {
+            int a=55, b= 100, c = 20;
+            if (a > b)
+            {
+                if (a > c)
+                {
+                    Console.WriteLine(a);
+                }
+                else
+                {
+                    Console.WriteLine(c);
+                }
+            }
+            else
+            {
+                if (b > c)
+                {
+                    Console.WriteLine(b);
+                }
+                else
+                {
+                    Console.WriteLine(c);
+                }
+            }
+        }
+
+        static void EligibleToVote()
+        {
+            Console.WriteLine("Enter Age");
+            int Age = Convert.ToInt32(Console.ReadLine());
+            if (Age < 18)
+            {
+                Console.WriteLine("Can not vote");
+            }
+            else
+            {
+                Console.WriteLine("Can Vote");
+            }
+        }
+        static void CheckDate()
+        {
+            Console.WriteLine("Inout year then month then day");
+            int year = Convert.ToInt32(Console.ReadLine());
+            int month = Convert.ToInt32(Console.ReadLine());
+            int day = Convert.ToInt32(Console.ReadLine());
+            DateTime dt = new DateTime(year,month,day);
+
+            DateTime todayDate = DateTime.Now;
+            if(DateTime.Compare(dt, todayDate) < 0)
+            {
+                Console.WriteLine("Before Todays Date");
+                
+            }
+            else
+            {
+                Console.WriteLine("After todays Date");
+            }
+            
+
+
+        }
+        static void LeapYear()
+        {
+            int year = 2019;
+            if (DateTime.IsLeapYear(year))
+            {
+                Console.WriteLine("Yes it is Leap Year!");
+            }
+            else
+            {
+                Console.WriteLine("Not it is not a Leap Year!");
+            }
         }
 
         static void NDiscount()
